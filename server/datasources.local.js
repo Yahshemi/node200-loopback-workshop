19ede31cd64f23256806df828a7b3da794e9a882
Yahshemi Walters <Yahshemi@gmail.com>
2019-01-31 14:58:23 -0800

    changed db to MongoDB in model-config

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,9 +1,11 @@
-module.exports ={
-    "db":{
-        "name":"db",
-        "connector":"memory"
+module.exports = {
+    "db": {
+        "name": "db",
+        "connector": "memory"
     },
-    "MongoDB":{
-        "host":"","port":0,"url":"","database":"","password":"",
-        "name":"MongoDB",
-        "user":"","connector":"mongodb"}}
+    "MongoDB": {
+        "url": "mongodb://localhost:27017",
+        "name": "MongoDB",
+        "connector": "mongodb"
+    }
+}
